6064892d0a60e8c4ba1e77ad7ebc9975e100e32a
Gagandeep Singh <deepgagan9336@gmail.com>
2021-12-22 13:32:40 +0530

    chegg-student-details

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-var express        = require("express"),
-    app            = express(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    Schema         = require("./models/Schema");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  Schema = require("./models/Schema");
 
-    const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-    mongoose.connect("mongodb://localhost:27017/cheggQA", { useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/cheggQA", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
-    app.set("view engine", "ejs");
-    app.use(express.urlencoded({extended:true}));
-    app.use(express.static(__dirname+"/public/"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public/"));
 
-    app.get("/", (req, res) => {
-        Schema.find({}, (err, allstudents) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("append_to_table", { student: allstudents});
-            }
-        })
-    });
+app.get("/", (req, res) => {
+  Schema.find({}, (err, allstudents) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("append_to_table", { student: allstudents });
+    }
+  });
+});
 
-    app.post("/", (req, res) => {
-        var student_name = req.body.student_name;
-        var student_id = req.body.student_id;
-        var course = req.body.course; 
+app.post("/", (req, res) => {
+  var student_name = req.body.student_name;
+  var student_id = req.body.student_id;
+  var course = req.body.course;
+  var grade = req.body.grade;
 
-        var student = {
-            student_name : student_name,
-            student_id : student_id,
-            course : course
-        }
+  var student = {
+    student_name: student_name,
+    student_id: student_id,
+    course: course,
+    grade: grade,
+  };
 
-        Schema.create(student, (err) => {
-            if(err) {
-                console.log(err);
-            } else {
-                res.redirect("/");
-            }
-        })
-    })
+  Schema.create(student, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
-    //port
-    app.listen(PORT, () => {
-        console.log(`The Server Started at Port ${PORT}`);
-    }); 
+//port
+app.listen(PORT, () => {
+  console.log(`The Server Started at Port ${PORT}`);
+});
--- models/Schema.js
+++ models/Schema.js
@@ -1,9 +1,10 @@
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 //schema Setup
 const Schema = new mongoose.Schema({
-    student_name: String,
-    student_id: String,
-    course: String,
+  student_name: String,
+  student_id: Number,
+  course: String,
+  grade: String,
 });
 
 module.exports = mongoose.model("Schema", Schema);
